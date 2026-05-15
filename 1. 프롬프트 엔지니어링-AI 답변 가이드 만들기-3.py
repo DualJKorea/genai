4bdc13f27825fa18797b4bb5de27958608523a8f
DualJKorea <dualjkorea@gmail.com>
2026-05-15 13:38:34 +0900

    Update 1. 프롬프트 엔지니어링-AI 답변 가이드 만들기-3.py

--- 1. 프롬프트 엔지니어링-AI 답변 가이드 만들기-3.py
+++ 1. 프롬프트 엔지니어링-AI 답변 가이드 만들기-3.py
@@ -6,7 +6,7 @@ _ = load_dotenv(find_dotenv())
 openai.api_key  = os.getenv('OPENAI_API_KEY')
 client = openai.OpenAI()
 
-def get_completion(prompt, model="gpt-3.5-turbo"):
+def get_completion(prompt, model="gpt-4.1-nano"):
     messages = [{"role": "user", "content": prompt}]
     response = client.chat.completions.create(
         model=model,
